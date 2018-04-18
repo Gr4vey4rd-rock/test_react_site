605adc2106bb61a3730bb3895ce8bca4bbd9b9bc
Jordan Peterson <gr4vey4rdrock@gmail.com>
2018-04-18 11:33:07 -0500

    test 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -34,8 +34,16 @@ class App extends Component {
         <p className="App-paragraph2">
 █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
         </p>
+        <p className="Variable">
+          "ya boi jack bob has been in this location"- jim bob
+        </p>
+        
+         <p className="Real-deep-stuff">
+         This is for an e@st Project so it might not look that good but hey the amount of code put into this gives up real respect for the people who do this for a profesion
+    </p>
+    <p className="unicorn">
 
-    
+    </p>
       </div>
     )
   }
